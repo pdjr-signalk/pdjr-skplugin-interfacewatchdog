7733019f5d89cb927f3f59725f34263cf8baed4b
Paul Reeve <preeve@pdjr.eu>
2023-10-23 11:34:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ module.exports = function(app) {
     app.debug(`using configuration: ${JSON.stringify(plugin.options, null, 2)}`);
   
     if (plugin.options.interfaces.length > 0) {
-      log.N(`watching interfaces ${JSON.stringify(plugin.options.interfaces.map(interface => (interface.name)))}`);
+      log.N(`watching interfaces ${plugin.options.interfaces.map(interface => (interface.name  + (interface.reboot)?'(R)':'')).join(', ')}`);
         
       plugin.options.interfaces.forEach(interface => {
         App.notify(interface.notificationPath, { state: 'normal', method: [], message: 'Waiting for interface to become active' }, plugin.id);
