103c06cddfcf1bb3d282815e11ae60ab948408a5
Paul Reeve <preeve@pdjr.eu>
2023-10-26 15:32:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -237,7 +237,7 @@ module.exports = function(app) {
 
   function changeState(watchdog, state) {
     watchdog.state = state;
-    watchdog.stateHistory.push(new Date().toISOString('YYYY-MM-DDTHH:mm:ss ') + state);
+    watchdog.stateHistory.push(new Date().toISOString('YYYY-MM-DDTHH:mm:ss') + ' ' + state);
   }
 
   function saveShadowOptions() {
